a498d100e1cf8bbc0ebfcae4764a777c36b82166
Jed-Miller <124761581+Jed-Miller@users.noreply.github.com>
2023-04-28 12:36:00 -0700

    updated schemata and started query

--- EmployeeSQL/employeeSQL_query.sql
+++ EmployeeSQL/employeeSQL_query.sql
@@ -1,58 +1,35 @@
---Drop tables if they exist
-DROP TABLE IF EXISTS departments CASCADE;
-DROP TABLE IF EXISTS titles CASCADE;
-DROP TABLE IF EXISTS employees CASCADE;
-DROP TABLE IF EXISTS dept_emp CASCADE;
-DROP TABLE IF EXISTS dept_manager CASCADE;
-DROP TABLE IF EXISTS salaries;
+--List the employee number, last name, first name, sex, and salary of each employee (2 points)
+SELECT e.emp_no as "Employee Number", 
+	e.last_name as "Last Name", 
+	e.first_name as "First Name",
+	e.sex as "Sex",
+	s.salary as "Salary"
+FROM employees e
+INNER JOIN salaries s
+ON e.emp_no = s.emp_no;
 
---Create departments table
-CREATE TABLE departments (
-    dept_no VARCHAR(4) NOT NULL PRIMARY KEY,
-    dept_name VARCHAR(30) NOT NULL
-);
-
---Create titles table
-CREATE TABLE titles (
-    title_id VARCHAR(5) NOT NULL PRIMARY KEY,
-    title VARCHAR(50)
-);
-
---Create employees table
-CREATE TABLE employees (
-    emp_no int PRIMARY KEY NOT NULL,
-    emp_title_id VARCHAR(5) NOT NULL,
-    birth_date DATE NOT NULL,
-    first_name VARCHAR(30) NOT NULL,
-    last_name VARCHAR(30) NOT NULL,
-    sex VARCHAR(2) NOT NULL,
-    hire_date DATE NOT NULL,
-    FOREIGN KEY (emp_title_id) REFERENCES titles(title_id)
-);
-
---Create dept_emp table
-CREATE TABLE dept_emp (
-    emp_no INT NOT NULL,
-    dept_no VARCHAR(5) NOT NULL,
-	PRIMARY KEY (emp_no, dept_no),
-	FOREIGN KEY (emp_no) REFERENCES employees(emp_no),
-	FOREIGN KEY (dept_no) REFERENCES departments(dept_no)
-);
-
---Create dept_manager table
-CREATE TABLE dept_manager (
-    dept_no VARCHAR(5) NOT NULL,
-	emp_no INT NOT NULL,
-	PRIMARY KEY (dept_no, emp_no),
-	FOREIGN KEY (emp_no) REFERENCES employees(emp_no),
-	FOREIGN KEY (dept_no) REFERENCES departments(dept_no)
-);
-
---Create salaries
-CREATE TABLE salaries (
-    emp_no INT NOT NULL PRIMARY KEY,
-    salary DEC NOT NULL,
-    FOREIGN KEY (emp_no) REFERENCES employees(emp_no)
-);
+--List the first name, last name, and hire date for the employees who were hired in 1986 (2 points)
 
 
+--List the manager of each department along with their department number, department name, 
+--employee number, last name, and first name (2 points)
+
+
+--List the department number for each employee along with that employee’s employee number, last name, 
+--first name, and department name (2 points)
+
+
+--List first name, last name, and sex of each employee whose first name is Hercules and whose last name 
+--begins with the letter B (2 points)
+
+
+--List each employee in the Sales department, including their employee number, last name, 
+--and first name (2 points)
+
+
+--List each employee in the Sales and Development departments, including their employee number, last name, 
+--first name, and department name (4 points)
+
+
+--List the frequency counts, in descending order, of all the employee last names (that is, how many employees 
+--share each last name) (4 points)
